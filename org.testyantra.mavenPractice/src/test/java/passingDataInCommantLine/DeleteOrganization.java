4e32b49538c4861d502c6f70202ab725c0e5a796
iqbal2214 <iqbalrimtian2214@gmai.com>
2022-03-23 16:45:32 +0530

    all data

---
+++ org.testyantra.mavenPractice/src/test/java/passingDataInCommantLine/DeleteOrganization.java
@@ -0,0 +1,11 @@
+package passingDataInCommantLine;
+
+import org.testng.annotations.Test;
+
+public class DeleteOrganization {
+	@Test
+	public void delete() {
+		System.out.println("deletation compleated");
+	}
+
+}
